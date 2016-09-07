f10af366227e761ff44f2157a51b6c2117ca95b5
Georgios Siviloglou <gsivil@gmail.com>
2016-09-08 00:27:31 +0200

    Rename input files

--- textpare.c
+++ textpare.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-#define textA "textA.txt"
-#define textB "textB.txt"
+#define textA "A.txt"
+#define textB "B.txt"
 
 int main(void)
 {
+    FILE* fpA;
+    FILE* fpB;
+
+
     printf("Compare %s to %s\n", textA, textB);
     return 0;
 }
