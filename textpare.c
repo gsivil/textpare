9794da05834fd3eb27fe7795a6085d8c56cb26ae
Georgios Siviloglou <gsivil@gmail.com>
2016-09-08 01:04:30 +0200

    print given number of words

--- textpare.c
+++ textpare.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define textA "A.txt"
 #define textB "B.txt"
 
 
 void printall(char text[]);
+void printword(char text[], unsigned int wordpos);
 
 int main(void)
 {
     printf("Compare %s to %s\n", textA, textB);
     printall(textA);
+    for (unsigned int i = 0; i<200; i++)
+    {
+        printword(textA, i);
+        putchar('\n');
+    };
     return 0;
 }
 
@@ -26,4 +33,24 @@ void printall(char text[])
     fclose(fp);
 }
 
+void printword(char text[], unsigned int wordpos)
+{
+    FILE* fp;
+    fp = fopen(text, "r");
+    char nextchar = getc(fp);
+    unsigned int curwordpos = 0;
+    while((nextchar != EOF) && (curwordpos<wordpos))
+    {
+        if(isalpha(nextchar))
+        {
+            putchar(nextchar);    
+        }
+        if (nextchar == ' ')
+        {
+            curwordpos = curwordpos+1;
+            putchar(nextchar);
+        };
+        nextchar = getc(fp);
+    }
+}
 
