94bc28c77727868e5b56ec1af8933e14034f866b
Georgios Siviloglou <gsivil@gmail.com>
2016-09-09 01:04:14 +0200

    problem with char 13

--- textpare.c
+++ textpare.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define textA "A.txt"
 #define textB "B.txt"
@@ -9,22 +10,31 @@
 void printall(char text[]);
 void printwords(char text[], unsigned int wordbeg, unsigned int wordend);
 void longline();
+void mywait(double seconds);
 
 int main(void)
 {
     printf("Compare %s to %s\n", textA, textB);
-    printall(textA);
     longline();
     unsigned int i = 0;
     unsigned int j = 100;
     printwords(textA, i, j);
+    putchar('\n');
     longline();
     return 0;
 }
+void mywait(double seconds)
+{
+    fflush(stdout);
+    usleep(1000000*seconds);
+};
 void longline()
 {
-    for (int i =0;i<80;i++)
+    for (int i =10;i<80;i++)
+    {
         printf("_");
+        mywait(0.01);
+    };
     printf("\n");
 }
 void printall(char text[])
@@ -47,28 +57,27 @@ void printwords(char text[], unsigned int wordbeg, unsigned int wordend)
     char nextchar = getc(fp);
     unsigned int curwordpos = 0;
     int tempspace = 0;
-    int tempnewline = 0;
     while(nextchar != EOF)
     { 
+        if (nextchar == 13)
+            nextchar = '\n';
+        else
+        {
         if(!isalnum(nextchar) && (!isspace(nextchar)) && (!ispunct(nextchar)))
         {
             nextchar = getc(fp);
-            if ((curwordpos<=wordend) && (curwordpos>=wordbeg))
-               // putchar(nextchar);
-               nextchar = getc(fp);
         };
-        if (isspace(nextchar))
-            nextchar = getc(fp);
-        if(((nextchar == '\n') || isalnum(nextchar) || (ispunct(nextchar)))&& (!isspace(nextchar)))
+        if((isalnum(nextchar) || (ispunct(nextchar)))&& (!isspace(nextchar)))
         {
-            if ((curwordpos<=wordend) && (curwordpos>=wordbeg))
+            if ((curwordpos<wordend) && (curwordpos>=wordbeg))
                 putchar(nextchar);    
             nextchar = getc(fp);
         }
-        if ((isspace(nextchar) || (nextchar == '\n')) && (tempspace <1))
+        if ((isspace(nextchar)) && (tempspace <1))
         {
+            mywait(0.01);
             curwordpos = curwordpos+1;
-            if ((curwordpos<=wordend) && (curwordpos>=wordbeg))
+            if ((curwordpos<wordend) && (curwordpos>=wordbeg))
                 putchar(nextchar);
             nextchar = getc(fp);
             while(isspace(nextchar))
@@ -76,7 +85,10 @@ void printwords(char text[], unsigned int wordbeg, unsigned int wordend)
                 nextchar = getc(fp);
                 tempspace = tempspace+1;
             };
-            tempspace = 0;
+        };
+        tempspace = 0;
+        if (curwordpos >= wordend)
+            break;
         };
     }
 }
