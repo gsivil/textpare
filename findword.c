1517e4ebb2094dd2a1e0417799058f11b0c90e52
Georgios Siviloglou <gsivil@gmail.com>
2016-09-12 22:48:41 +0200

    print a given number of chars/words after found word

--- findword.c
+++ findword.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define textin "A.txt"
 
 void printall(char text[], char* theword);
 
 
-int main(void)
+int main(int argc, char* argv[])
 {
-    printall(textin, "oo"); 
+    char* theword;
+    if (argv[1] == '\0')
+        theword = "a";
+    else
+        theword = argv[1];
+
+    theword = "ing";
+        printall(textin, theword); 
+    putchar('\n');
     return 0;
 }
 
@@ -23,19 +32,19 @@ void printall(char text[], char* theword)
     int checkword = 1;
     int i = 0;
     int wordlen = strlen(theword);
+    int words = 0;
     printf("%d", wordlen);
     while (nextchar != EOF)
     {
         nextchar = getc(fp);
         checkword = 1;
         i = 0;
-        while ((i<wordlen) && (checkword))
+        while ((i<wordlen) && (checkword) && (nextchar != EOF))
         {
             nextchar = getc(fp);
             if (nextchar == theword[i])
             {
                 checkword = checkword*1;
-                printf("%c", nextchar);
             }
             else
             {
@@ -45,20 +54,21 @@ void printall(char text[], char* theword)
         };
         if (checkword)
         {
-
-        //    printf("%s", theword);
-/*            for (int i=0;i<15;i++)
+            words = 0;
+            printf("%s", theword);
+            for (int i=0;((i<150) && (words<7) && (nextchar != EOF));i++)
             {
                 nextchar = getc(fp);
+                if (isspace(nextchar))
+                    words = words+1;
                 printf("%c", nextchar);
             };
             putchar(' '); 
-            */
             permitnewline = 1;
         }
         else if ((nextchar !='\n') && (permitnewline == 1))
             printf(" ");
-        if ((nextchar == '\n') && (permitnewline == 1))
+        if ((nextchar == '\n') && (permitnewline == 0))
         {
             permitnewline = 0;
             printf("\n");
